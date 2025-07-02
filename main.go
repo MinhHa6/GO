ca5295e96291039934090865bbe75a304421c9bf
Minh Ha <minhha1192003@gmail.com>
2025-07-02 23:47:53 +0700

    Khai bao nhieu du lieu

--- main.go
+++ main.go
@@ -9,4 +9,11 @@ func main() {
 	fmt.Println(os.Getenv("App_name"))
 	var a1 int = 10
 	fmt.Print(a1)
+	var (
+		a2 int
+		a3 string
+		a4 float32
+	)
+	a2, a3, a4 = 20, "Hello", 3.4
+	fmt.Println(a2, a3, a4)
 }
